bb0d4548a22aa50cf5e3934ea91007202dd49fcc
shu-iren <167405152+shu-iren@users.noreply.github.com>
2024-04-18 16:28:07 +0900

    Add files via upload

--- chatbot.js
+++ chatbot.js
@@ -5,7 +5,7 @@ const userInput = document.querySelector('#user-input input');
 // 전송 버튼
 const sendButton = document.querySelector('#user-input button');
 // 발급받은 OpenAI API 키를 변수로 저장
-const apiKey = '발급받은 API키 입력';
+const apiKey = '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';
 // OpenAI API 엔드포인트 주소를 변수로 저장
 const apiEndpoint = 'https://api.openai.com/v1/chat/completions'
 function addMessage(sender, message) {
@@ -33,12 +33,12 @@ async function fetchAIResponse(prompt) {
                 role: "user", // 메시지 역할을 user로 설정
                 content: prompt // 사용자가 입력한 메시지
             }, ],
-            temperature: 0.8, // 모델의 출력 다양성
-            max_tokens: 1024, // 응답받을 메시지 최대 토큰(단어) 수 설정
-            top_p: 1, // 토큰 샘플링 확률을 설정
-            frequency_penalty: 0.5, // 일반적으로 나오지 않는 단어를 억제하는 정도
-            presence_penalty: 0.5, // 동일한 단어나 구문이 반복되는 것을 억제하는 정도
-            stop: ["Human"], // 생성된 텍스트에서 종료 구문을 설정
+            temperature: 0.99, // 모델의 출력 다양성
+            max_tokens: 300, // 응답받을 메시지 최대 토큰(단어) 수 설정
+            top_p: 0.98, // 토큰 샘플링 확률을 설정
+            frequency_penalty: 0.02, // 일반적으로 나오지 않는 단어를 억제하는 정도
+            presence_penalty: 0, // 동일한 단어나 구문이 반복되는 것을 억제하는 정도
+            stop: ["ㅋㅋㅋㅋㅋㅋㅋㅋ"], // 생성된 텍스트에서 종료 구문을 설정
         }),
     };
     // API 요청후 응답 처리
